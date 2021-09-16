32756d9c645ab4241744282564d537d9ce4e15c1
bollma13 <90811136+bollma13@users.noreply.github.com>
2021-09-16 18:10:57 -0400

    Update dash_practice.py

--- dash_practice.py
+++ dash_practice.py
@@ -46,6 +46,6 @@ app.layout = html.Div(
 
 
 if __name__ == '__main__':
-    app.run_server(port=1029, debug = True)
+    app.run_server(debug = True)
                                  
                       
